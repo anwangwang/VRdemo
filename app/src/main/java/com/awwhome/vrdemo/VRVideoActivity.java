2bdce576eae8abf82e4d7303f879b9b3c2f306e0
anwangwang <wangwang@tc2025.com>
2017-02-06 19:03:08 +0800

    1、导入官方库 2、全景图片的开发

---
+++ app/src/main/java/com/awwhome/vrdemo/VRVideoActivity.java
@@ -0,0 +1,17 @@
+package com.awwhome.vrdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VRVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vr_video);
+
+        // 全景视频的浏览功能
+
+
+    }
+}
